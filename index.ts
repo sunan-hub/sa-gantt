db197428020075c216899bfa61773fbb29ad4dd7
XP-PC-SUNA1\suna1 <@Sa19991023>
2024-07-23 09:14:48 +0800

    feat: 导出组件

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 import page from "./src/pages/index";
+import GanttTable from "./src/components/gantt-table";
+import GanttRow from "./src/components/gantt-row";
+
+// 导出甘特图组件
+export { GanttRow, GanttTable };
+
+// 导出类型
+export * from './src/types/gantt';
 
 export default page;
